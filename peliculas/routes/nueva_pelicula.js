733b80e73a91f36ed3726e6e163f4968cec4d1af
CatalinaMariana <47732253+CatalinaMariana@users.noreply.github.com>
2020-06-16 01:12:44 -0500

    modifiquePelicula_nueva

--- peliculas/routes/nueva_pelicula.js
+++ peliculas/routes/nueva_pelicula.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('nueva_pelicula', { title: 'Nueva película' });
 });
 
+
+//POST
+router.pos('/',function(req, res, next) => {
+ var pelicula = Pelicula({
+   id:  req.body.id,
+   name:  req.body.name,
+   age: req.body.ag,
+   director: req.body.director,
+   gender: req.body.genero
+ });
+ pelicula.save(error,data) => {
+   if (error) res.status(404).json({mensaje:'No se guardo correctamente'})
+   else res status(201).json(data);
+ });
+  //
+});
+
+
 module.exports = router;
